21dafca351479fecbbbbf7ca89598e7e78fd2f42
Nastassie <50500307+Nastassie@users.noreply.github.com>
2019-07-08 15:40:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+const url = 'https://api.myjson.com/bins/152f9j';
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        const rawData = data.data;
+        return rawData.map(post => {
+          //all needed data is listed below as an entity 
+          let createdAt = post.createdAt;
+                description = post.description;
+                img = post.image;
+                tags = post.tags;
+                title = post.title;
+            //create element
+            //append element
+        });
+    })
+    .catch((error) => {
+        console.log(JSON.stringify(error));
+    });
