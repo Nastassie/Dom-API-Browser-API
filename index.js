16ad47ec2852a0421ac7161f9d076930ca741d28
Nastassie <50500307+Nastassie@users.noreply.github.com>
2019-07-09 12:34:28 +0300

    Adding content

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const url = 'https://api.myjson.com/bins/152f9j';
+const container = document.getElementById('cont');
+//const itemdate = document.getElementById('pdate');
 fetch(url)
     .then(res => res.json())
     .then(data => {
@@ -10,10 +12,38 @@ fetch(url)
                 img = post.image;
                 tags = post.tags;
                 title = post.title;
-            //create element
-            //append element
+                //create element
+                article = document.createElement('div');
+                postdiv = document.createElement('div');
+                posttitle = document.createElement('h1');
+                posttags = document.createElement('p');
+                postimg = document.createElement('img');
+                postdate = document.createElement("p");
+                //post.setAttribute('class', 'post_class')
+                postdiv.innerHTML = description;
+                posttitle.innerHTML = title;
+                posttags.innerHTML = tags;
+                //posttags.split("");
+                postdiv.setAttribute("class", "description");
+                postimg.setAttribute("src", img);
+                postimg.setAttribute("height", "150px");
+                postimg.setAttribute("width", "150px");
+                postdate.setAttribute("class", "pdate");
+                posttags.setAttribute("class", "tags");
+                postdate.innerHTML = createdAt;
+                article.setAttribute("class", "article");
+                
+                //append element
+                article.appendChild(posttitle);
+                article.appendChild(postimg);
+                article.appendChild(postdiv);
+                article.appendChild(postdate);
+                article.appendChild(posttags);
+                container.appendChild(article);
+
         });
     })
+    //.then(arr => (arr))
     .catch((error) => {
         console.log(JSON.stringify(error));
     });
