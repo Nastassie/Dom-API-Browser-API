87c7ddb601e3dfcddaf65f0d22eea070b74679d9
Nastassie <50500307+Nastassie@users.noreply.github.com>
2019-07-10 09:26:26 +0300

    Add scrolling

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const url = 'https://api.myjson.com/bins/152f9j';
 const container = document.getElementById('cont');
+
+function removeArticle(elem){
+    elem.parentElement.setAttribute("class", "removedarticle");
+    var elements = document.getElementsByClassName("hiddenarticle");
+    if (elements.length)
+        elements[0].setAttribute("class", "article");
+}
 //const itemdate = document.getElementById('pdate');
 fetch(url)
     .then(res => res.json())
@@ -13,6 +20,9 @@ fetch(url)
                 tags = post.tags;
                 title = post.title;
                 //create element
+                button_r = document.createElement('button')
+                button_r.setAttribute("class", "delete");
+                button_r.setAttribute("onClick", "removeArticle(this)");
                 article = document.createElement('div');
                 postdiv = document.createElement('div');
                 posttitle = document.createElement('h1');
@@ -22,28 +32,101 @@ fetch(url)
                 //post.setAttribute('class', 'post_class')
                 postdiv.innerHTML = description;
                 posttitle.innerHTML = title;
-                posttags.innerHTML = tags;
-                //posttags.split("");
+                posttags.innerHTML = tags.join(", ");
+                //print(tags.split(","));
                 postdiv.setAttribute("class", "description");
                 postimg.setAttribute("src", img);
                 postimg.setAttribute("height", "150px");
                 postimg.setAttribute("width", "150px");
                 postdate.setAttribute("class", "pdate");
                 posttags.setAttribute("class", "tags");
-                postdate.innerHTML = createdAt;
-                article.setAttribute("class", "article");
+                dt = new Date(createdAt);
+                postdate.innerHTML = dt.toLocaleString();
+                //article.setAttribute("class", "article");
                 
                 //append element
+                article.appendChild(button_r);
                 article.appendChild(posttitle);
                 article.appendChild(postimg);
                 article.appendChild(postdiv);
                 article.appendChild(postdate);
                 article.appendChild(posttags);
-                container.appendChild(article);
-
+                //article.style.visibility = "hidden";
+                //container.appendChild(article);
+                return article;
         });
     })
+    .then(elArr => {
+            for (i = 0; i < elArr.length; ++i)
+            {
+                article = elArr[i];
+                if (i < 10)
+                {
+                    article.setAttribute("class", "article");
+                }
+                else
+                {
+                    article.setAttribute("class", "hiddenarticle");
+                }
+                container.appendChild(article);
+            }
+        }
+    )
     //.then(arr => (arr))
     .catch((error) => {
         console.log(JSON.stringify(error));
     });
+
+window.onscroll = (function() {
+   if(window.scrollY + window.innerHeight >= document.body.clientHeight) {
+       var list = document.getElementsByClassName("hiddenarticle");
+       if (!list)
+            return;
+       list = Array.from(list).slice(0, 10);
+       list.forEach(function(element) {
+           element.setAttribute("class", "article");
+       });
+       
+   }
+});
+
+   document.getElementById("reset").onclick = function (){
+       window.scrollTo(0, 0);
+       list = Array.from(document.getElementsByClassName("article"));
+       for (i = 10; i < list.length; ++i)
+       {
+        list[i].setAttribute("class", "hiddenarticle");
+       }
+   
+   }
+/*
+
+
+var date_sort_asc = function (date1, date2) {
+  // This is a comparison function that will result in dates being sorted in
+  // ASCENDING order. As you can see, JavaScript's native comparison operators
+  // can be used to compare dates. This was news to me.
+  if (date1 > date2) return 1;
+  if (date1 < date2) return -1;
+  return 0;
+};
+
+var date_sort_desc = function (date1, date2) {
+  // This is a comparison function that will result in dates being sorted in
+  // DESCENDING order.
+  if (date1 > date2) return -1;
+  if (date1 < date2) return 1;
+  return 0;
+};
+
+
+.sort(date_sort_asc);
+for (var i = 0; i < postdate.length; i++){
+
+}
+
+article.sort(date_sort_desc);
+for (var i = 0; i < postdate.length; i++) {
+  
+}
+*/
